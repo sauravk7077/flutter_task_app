dd679ecc3f5970eb62f5b1cd94855d8d6d473073
Saurav Kumar <sauravk7077@gmail.com>
2020-12-15 11:11:43 +0530

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,10 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 // import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_task_app/main.dart';
 
-import 'package:flutter_task_app/main.dart';
+import 'package:test/test.dart';
 
 void main() {
   test('Not a test', () {
