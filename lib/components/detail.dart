c6f39f0db5280ed994658d1e143d79d2ea9864ab
Brady Trainor <git_commit@tangential.info>
2021-01-01 00:00:00 +0000

    Add some minimal boilerplate for Detail page

---
+++ lib/components/detail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:taskc/taskc.dart';
+
+class Detail extends StatelessWidget {
+  Detail(Task task);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
